96092c11b8123e4ed440921680759772bf2a6bce
Adolfo Santiago <epoch@adol.pw>
2024-04-05 19:13:54 +0200

    Update Android Studio flake

--- external/editors/android-studio/default.nix
+++ external/editors/android-studio/default.nix
@@ -9,16 +9,16 @@ let
       inherit tiling_wm;
     };
   stableVersion = {
-    version = "2023.2.1.24"; # "Android Studio Iguana | 2023.2.1 (Patch 1)"
+    version = "2023.2.1.24"; # "Android Studio Iguana | 2023.2.1 Patch 1"
     sha256Hash = "sha256-ACZCdXKEnJy7DJTW+XGOoIvDRdzP47NytUEAqV//mbU=";
   };
   betaVersion = {
-    version = "2023.2.1.24"; # "Android Studio Iguana | 2023.2.1 (Patch 1)"
-    sha256Hash = "sha256-ACZCdXKEnJy7DJTW+XGOoIvDRdzP47NytUEAqV//mbU=";
+    version = "2023.3.1.15"; # "Android Studio Jellyfish | 2023.3.1.1 Beta 2"
+    sha256Hash = "sha256-ImXHda8Xbayuk+OMZVtAFsGNnaqm2PvI3lko2gUpIeU=";
   };
   latestVersion = {
-    version = "2023.2.1.24"; # "Android Studio Iguana | 2023.2.1 (Patch 1)"
-    sha256Hash = "sha256-ACZCdXKEnJy7DJTW+XGOoIvDRdzP47NytUEAqV//mbU=";
+    version = "2024.1.1.1"; # "Android Studio Koala | 2024.1.1 Canary 3"
+    sha256Hash = "sha256-QNAudFlM+1QAZg+EYgiIknllai4N1wj55ZnkUWho7ps=";
   };
 in {
   # Attributes are named by their corresponding release channels
