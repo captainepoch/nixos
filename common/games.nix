6cd10eb6e52f419a77d99ed00d1f190023e62ad6
Adolfo Santiago <epoch@adol.pw>
2025-01-05 07:09:19 +0100

    Changes into games flake

--- common/games.nix
+++ common/games.nix
@@ -4,7 +4,7 @@
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [ "steam" "steam-original" "steam-run" ];
 
-  environment.systemPackages = with pkgs; [ lutris xboxdrv xorg.xinput ];
+  environment.systemPackages = with pkgs; [ lutris xboxdrv xorg.xinput wine ];
 
   programs.steam = {
     enable = true;
