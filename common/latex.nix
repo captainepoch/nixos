adf5f19b77bfed0a2701f555ce0a7361d06e32db
Adolfo Santiago <epoch@adol.pw>
2025-05-31 10:58:57 +0200

    Update LaTeX environment

--- common/latex.nix
+++ common/latex.nix
@@ -4,8 +4,9 @@ let
   tex = (pkgs.texlive.combine {
     inherit (pkgs.texlive)
       amsmath capt-of dvisvgm dvipng # for preview and export as html
-      enumitem hyperref paracol pdfx preprint scheme-medium ulem wrapfig
-      xmpincl;
+      enumitem hyperref paracol pdfx preprint scheme-medium ulem wrapfig xmpincl
+      xifthen ifmtarg xstring fontawesome5 roboto sourcesanspro tcolorbox
+      tikzfill fontspec;
   });
 
 in { environment.systemPackages = with pkgs; [ tex texstudio ]; }
