8f8e42c4a0548fe7d9f2cf0a108b7c263723d51f
Adolfo Santiago <epoch@adol.pw>
2025-12-21 11:57:35 +0100

    Enable printing

--- common/printing.nix
+++ common/printing.nix
@@ -3,6 +3,31 @@
 {
   environment.systemPackages = with pkgs; [ xsane ];
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+    publish = {
+      enable = true;
+      userServices = true;
+    };
+  };
+
+  services.printing = {
+    enable = true;
+    browsing = true;
+    defaultShared = true;
+    openFirewall = true;
+    drivers = with pkgs; [ brlaser cups-filters cups-browsed gutenprint ];
+  };
+
+  hardware.printers = {
+    ensurePrinters = [{
+      name = "Brother_DCP-L2627DWE";
+      location = "Home";
+      deviceUri = "ipp://10.0.7.41/ipp/print";
+      model = "everywhere";
+    }];
+    ensureDefaultPrinter = "Brother_DCP-L2627DWE";
+  };
 }
